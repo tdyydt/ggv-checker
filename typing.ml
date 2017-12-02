59c7c422b68fbcddaba9520413112560a804ed42
Yuya Tsuda <yytsuda@gmail.com>
2017-12-02 22:40:19 +0900

    Use VarSet & assert_disjoint

--- typing.ml
+++ typing.ml
@@ -1,6 +1,16 @@
 open Syntax
 module E = Environment
 
+(* set of variable *)
+(* IdSet, VarSet Variables *)
+module VarSet =
+  Set.Make(
+      struct
+        type t = id
+        let compare = compare
+      end
+    )
+
 exception Typing_error of string
 let ty_err s = raise (Typing_error s)
 
@@ -66,30 +76,44 @@ let matching_wait = function
   | _ -> ty_err "matching error: wait"
 
 
+let assert_disjoint xs ys =
+  let zs = VarSet.inter xs ys in
+  if VarSet.is_empty zs then ()
+                               (* zs を表示すれば良さそう。 *)
+                               (* violation of linearity *)
+                               (* 関数の名前から、specific すぎるエラーはどうなのか？ *)
+  else ty_err "Not disjoint"
+(* VarSet.t -> VarSet.t -> bool *)
+(* let is_disjoint xs ys =
+ *   VarSet.is_empty (VarSet.inter xs ys) *)
+
+
 (* TODO: 型付けに用いた linear 変数の集合を返す。
  * ように改良する *)
-(* tyenv -> exp -> ty * (id list) *)
+(* returns the set of linear type variables used in typing *)
+(* tyenv -> exp -> ty * VarSet.t *)
 let rec ty_exp tyenv = function
-  | Var x ->
-     try
-       let t = E.find x tyenv in
-       if un_tyenv (E.remove x tyenv) then
-         (* Set を使うべき？要検討 *)
-         if lin t then (v, [x])
-         else (v, [])
-       else ty_err "violation of linearity"
-     with
-     | Not_found -> ty_err "the variable is not bound"
+  | Var x -> begin
+      try
+        let t = E.find x tyenv in
+        if un_tyenv (E.remove x tyenv) then
+          (* Set を使うべき？要検討 *)
+          if lin t then (t, VarSet.singleton x)
+          else (t, VarSet.empty)
+        else ty_err "violation of linearity"
+      with
+      | Not_found -> ty_err "the variable is not bound"
+    end
   | Konst KUnit ->
-     if un_tyenv tyenv then TyUnit
+     if un_tyenv tyenv then (TyUnit, VarSet.empty)
      (* tyenv の中の linear な変数が捨てられることになり、
       * それを咎める *)
      else ty_err "violation of linearity"
   | Konst (KInt _) ->
-     if un_tyenv tyenv then TyInt
+     if un_tyenv tyenv then (TyInt, VarSet.empty)
      else ty_err "violation of linearity"
   | Konst (KBool _) ->
-     if un_tyenv tyenv then TyBool
+     if un_tyenv tyenv then (TyBool, VarSet.empty)
      else ty_err "violation of linearity"
 
   | BinOp (op, e1, e2) ->
@@ -99,9 +123,9 @@ let rec ty_exp tyenv = function
       * Lt とかもありえる
       * t1,t2 が int か確かめる等
       * LAnd 等も binop に入れて良いのでは？ *)
-     (* assert_disjoint xs ys; *)
-     (* yn_tyenv tyenv ?? *)
-     (TyInt, xs @ ys)
+     assert_disjoint xs ys;
+     (TyInt, VarSet.union xs ys)
+     (* un_tyenv tyenv ?? *)
        (* or typing error *)
 
 (*
@@ -111,21 +135,24 @@ let rec ty_exp tyenv = function
      else ty_err ""
  *)
   | Fun (Lin,x,t,e) ->
-     let u = ty_exp (E.add x t tyenv) e in
-     TyFun (Lin,t,u)
+     let u, ys = ty_exp (E.add x t tyenv) e in
+     (* remove x from ys, if included *)
+     (TyFun (Lin,t,u), VarSet.remove x ys)
   | Fun (Un,x,t,e) ->
-     let u = ty_exp (E.add x t tyenv) e in
-     if un_tyenv tyenv then TyFun (Un,t,u)
+     let u, ys = ty_exp (E.add x t tyenv) e in
+     if un_tyenv tyenv
+     then (TyFun (Un,t,u), VarSet.remove x ys)
      (* gamma に入っているだけで、実際に使われているかは、言い切れない *)
      else ty_err "unrestricted functions cannot contain variables of a linear type"
 
   | App (e1,e2) ->
-     let t1 = ty_exp tyenv e1 in
-     let t2 = ty_exp tyenv e2 in
+     let t1, xs = ty_exp tyenv e1 in
+     let t2, ys = ty_exp tyenv e2 in
      begin
        match matching_fun t1 with
        | TyFun (m, t11, t12) ->
-          if con_sub_ty t2 t11 then t12
+          if con_sub_ty t2 t11
+          then (t12, VarSet.union xs ys)
           else ty_err "not consistent subtype"
        (* matching fun とかで、すでに弾かれていて
         * ここまでエラーが来ないかも。
@@ -133,47 +160,45 @@ let rec ty_exp tyenv = function
        | _ -> assert false
      end
 
-  (* TODO: @ じゃなくて、set_union になるべきなのかもしれん。
-   * そうではない, disjoint であることを、先に確かめているため *)
   | ConsPair (Lin,e1,e2) ->
      let t1, xs = ty_exp tyenv e1 in
      let t2, ys = ty_exp tyenv e2 in
-     (* assert_disjoint vars1 vars2; *)
-     (TyProd (Lin,t1,t2), xs @@ ys)
+     assert_disjoint xs ys;
+     (TyProd (Lin,t1,t2), VarSet.union xs ys)
 
   | ConsPair (Un,e1,e2) ->
      let t1, xs = ty_exp tyenv e1 in
      let t2, ys = ty_exp tyenv e2 in
-     (* assert_disjoint vars1 vars2 *)
+     assert_disjoint xs ys;
      if un t1 && un t2
-     then (TyProd (Un,t1,t2), xs @@ ys)
+     then (TyProd (Un,t1,t2), VarSet.union xs ys)
      else ty_err "unrestricted pairs cannot contain linear varialbes"
 
   (* f は使わないべき？？統一性のため *)
   | DestPair (x,t1,y,t2,e1,e2) ->
-     let t1, vars1 = ty_exp tyenv e1 in
+     let t1, zs = ty_exp tyenv e1 in
      (* extend (x,t1) (y,t2) in tyenv *)
      todo ()
 
   | Fork e ->
-     let t, vars = ty_exp tyenv e in
+     let t, xs = ty_exp tyenv e in
      begin
        match matching_unit t with
-       | TyUnit -> (TyUnit, vars)
+       | TyUnit -> (TyUnit, xs)
        | _ -> assert false
      end
 
   | New -> todo ()
 
   | Send (e1,e2) ->
-     let t1, vars1 = ty_exp tyenv e1 in
-     let t2, vars2 = ty_exp tyenv e2 in
-     (* assert_disjoint vars1 vars2; *)
+     let t1, xs = ty_exp tyenv e1 in
+     let t2, ys = ty_exp tyenv e2 in
+     assert_disjoint xs ys;
      begin
        match matching_send t2 with
        | TySession (TySend (t3,s)) ->
           if con_sub_ty t1 t3
-          then (TySession s, vars1 @@ vars2)
+          then (TySession s, VarSet.union xs ys)
           else ty_err "Not consistent subtype"
        | _ -> assert false
      end
